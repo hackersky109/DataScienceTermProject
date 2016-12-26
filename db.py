c4c068eb0a0b78ffbb9b22dd8ca0949a3e120810
hackersky109 <hackersky109@gmail.com>
2016-12-26 23:57:30 +0800

    1.Now can load .csv and parse the trip path saving to mongodb

--- db.py
+++ db.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+import pymongo
+import csv
+import os
+import sys
 
-db = client['termproject']
-collect = db['test']
 
 
 def add(From, To):
@@ -13,3 +14,32 @@ def add(From, To):
         print post_id
     else:
         collect.update_one({'_id': post["_id"]}, {'$inc': {'Count': 1}})
+def get(From):
+    # for doc in collect.find({"From":From}).sort("Count",pymongo.ASCENDING):
+    return collect.find({"From":From}).sort("Count",pymongo.DESCENDING)[0]
+
+
+client = MongoClient('localhost', 27017)
+db = client['termproject']
+collect = db['test']
+
+datapath = 'D:\\workspace\\DataScience\\TermProject'
+os.chdir(datapath)
+if os.getcwd() != datapath:
+    print "EEROR: the file path incorrect."
+    sys.exit()
+
+f = open(datapath+'\data.csv', 'r')
+for row in csv.DictReader(f):
+    list = row['path'].split(',')
+    for i in range(0, len(list)-1, +1):
+        add(list[i],list[i+1])
+f.close()
+
+
+
+
+
+
+# a = get("Taipei")
+# print(a)
